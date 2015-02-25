6d885b40525b42c1898ace6a8388757069c0f066
Clinton Kitson <clintonskitson@gmail.com>
2015-02-25 15:47:46 -0800

    added multithreading

--- go1billion.go
+++ go1billion.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -27,28 +28,33 @@ func main() {
 
 func billion(res http.ResponseWriter, req *http.Request) {
 
-	startingTime := time.Now().UTC()
-	fmt.Println(startingTime)
-	CountTo = 1000000000
-	CountToPer := math.Floor(CountTo / float64(NumCPU))
+	requestNumCPU, _ := strconv.Atoi(req.URL.Query().Get("numcpu"))
 
+	if requestNumCPU > NumCPU || requestNumCPU < 1 {
+		requestNumCPU = 1
+	}
+
+	CountTo = 1000000000
+	CountToPer := int(math.Floor(CountTo / float64(requestNumCPU)))
+	fmt.Println(CountToPer)
 	var wg sync.WaitGroup
-	for i := 0; i < NumCPU-1; i++ {
+	startingTime := time.Now().UTC()
+	for i := 0; i < requestNumCPU; i++ {
 		wg.Add(1)
 		go func() {
-			for i := float64(0); i < CountToPer; i++ {
+			fmt.Println(fmt.Sprintf("started %v", CountToPer))
+			for i := 0; i < CountToPer; i++ {
 			}
 			wg.Done()
 		}()
-		fmt.Println("started 1")
+
 	}
 
 	wg.Wait()
-
 	endingTime := time.Now().UTC()
 
 	var duration time.Duration = endingTime.Sub(startingTime)
 
-	fmt.Fprintln(res, fmt.Sprintf(`<html><head><title>Go counts to %.0f</title></head><body><h1>Go counts to 1 billion</h1><p>This is how long it took me to count to 1billion, with %v CPUs:</p><p>%vms</p></body></html>`, CountTo, NumCPU, duration.Nanoseconds()/1e6))
+	fmt.Fprintln(res, fmt.Sprintf(`<html><head><title>Go counts to %.0f</title></head><body><h1>Go counts to 1 billion</h1><p>This is how long it took me to count to 1billion, with %v CPUs:</p><p>%vms</p></body></html>`, CountTo, requestNumCPU, duration.Nanoseconds()/1e6))
 
 }
