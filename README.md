428d4eba0df7133d01b7ba6b84750e155500f621
Clinton Kitson <clintonskitson@gmail.com>
2015-02-25 15:14:57 -0800

    added max cpu

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # go1billion
+
+
+#Check the results
+[http://go1billion.cfapps.io/](http://go1billion.cfapps.io/)
+
+
+# Push to Cloud Foundry!
+Modify it with a custom name of course..
+
+    git clone https://github.com/clintonskitson/go1billion
+    cd go1billion
+    ./cf push go1billion
--- go1billion.go
+++ go1billion.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
+	"os"
+	"runtime"
+	"sync"
 	"time"
 )
 
+var NumCPU int
+var CountTo float64
+
 func main() {
+	NumCPU = runtime.NumCPU()
+
+	runtime.GOMAXPROCS(NumCPU)
 	http.HandleFunc("/", billion)
 	fmt.Println("listening...")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
 	}
@@ -18,19 +28,27 @@ func main() {
 func billion(res http.ResponseWriter, req *http.Request) {
 
 	startingTime := time.Now().UTC()
+	fmt.Println(startingTime)
+	CountTo = 1000000000
+	CountToPer := math.Floor(CountTo / float64(NumCPU))
 
-	for i := 0; i < 1000000000; i++ {
-
+	var wg sync.WaitGroup
+	for i := 0; i < NumCPU-1; i++ {
+		wg.Add(1)
+		go func() {
+			for i := float64(0); i < CountToPer; i++ {
+			}
+			wg.Done()
+		}()
+		fmt.Println("started 1")
 	}
+
+	wg.Wait()
 
 	endingTime := time.Now().UTC()
 
 	var duration time.Duration = endingTime.Sub(startingTime)
 
-	fmt.Fprintln(res, fmt.Sprintf("Native [%v]\nMilliseconds [%d]\nSeconds [%.3f]\n",
-		duration,
-		duration.Nanoseconds()/1e6,
-		duration.Seconds()))
+	fmt.Fprintln(res, fmt.Sprintf(`<html><head><title>Go counts to %.0f</title></head><body><h1>Go counts to 1 billion</h1><p>This is how long it took me to count to 1billion, with %v CPUs:</p><p>%vms</p></body></html>`, CountTo, NumCPU, duration.Nanoseconds()/1e6))
 
-	//	fmt.Fprintln(res, fmt.Sprintf(`<!DOCTYPE html><html><head><title>GO counts to 1 billion</title><link rel="stylesheet" href="/stylesheets/style.css"></head><body><h1>Go counts to 1 billion</h1><p>This is how long it took me to count to 1billion:</p><p>xx</p></body></html>`))
 }
